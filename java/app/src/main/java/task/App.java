33b84dc754bdf64c5fb4745345782176bbbc85de
dominikocsofszki <DominikOcsofszki@gmail.com>
2023-10-22 19:46:07 +0200

    App for testing, one sample Record ready

--- java/app/src/main/java/task/App.java
+++ java/app/src/main/java/task/App.java
@@ -1,22 +1,22 @@
-//package task;
-//// import task.MongoDB;
-///**
-// * Hello world!
-// *
-// */
-//public class App
-//{
-//    public static void main( String[] args )   {
-//        System.out.println( "Hello World!" );
-//        ManagePersonalImpl managePersonal = new ManagePersonalImpl();
-//
-////        SocialPerfomanceRecord socialPerfomanceRecord =
-////                new SocialPerfomanceRecord(1, 2, 3, 4, 5, 6, 7, 8, 9)
-////        EvaluationRecord evaluationRecord = new EvaluationRecord(
-////
-////                new OrderEvaluation(1, 2, 3, 4, 5, 6, 7, 8, 9),
-////                1, 1, 1);
-//        managePersonal.createSalesMan();
-//
-//    }
-//}
+package task;
+// import task.MongoDB;
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )   {
+        System.out.println( "Hello World!" );
+        ManagePersonalImpl managePersonal = new ManagePersonalImpl();
+
+        SalesMan salesMan = new SalesMan("Dominik", "Ocsofszki", "IT");
+        final SocialAttribute socialAttribute = new SocialAttribute("better next time", 5, 4);
+        final SocialPerfomanceRecord socialPerfomanceRecord = new SocialPerfomanceRecord(socialAttribute, 2_000);
+        final Order order = new Order("HooverGo", "TKMax", "A",200,20,"last year we had 10 more sold");
+        final OrderEvaluation orderEvaluation = new OrderEvaluation(order, 1_000);
+        final EvaluationRecord evaluationRecord = new EvaluationRecord(socialPerfomanceRecord, orderEvaluation, 1, 3_000, 2023);
+        managePersonal.createSalesMan(salesMan);
+
+    }
+}
--- java/app/src/main/java/task/Order.java
+++ java/app/src/main/java/task/Order.java
@@ -3,15 +3,15 @@ package task;
 public class Order {
     final String productName;
     final String client;
-    final int clientRanking;
+    final String clientRanking;
     final int bonus;
     final int itemAmount;
     final String comment;
 
-    public Order(String productName, String client, int clientRanking, int bonus, int itemAmount, String comment) {
+    public Order(String productName, String client, String clientRanking, int bonus, int itemAmount, String comment) {
         this.productName = productName;
         this.client = client;
-        this.clientRanking = clientRanking;
+        this.clientRanking = clientRanking; // Could be changed to enum //ToDo change to enum?
         this.bonus = bonus;
         this.itemAmount = itemAmount;
         this.comment = comment;
