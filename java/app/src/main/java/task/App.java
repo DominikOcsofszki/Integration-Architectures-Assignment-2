c7a6f1d527a939c9d27b638eadb90fea15a17537
simonbiwer <91076673+simonbiwer@users.noreply.github.com>
2023-10-24 11:52:38 +0200

    implemented simple cli ui

--- java/app/src/main/java/task/App.java
+++ java/app/src/main/java/task/App.java
@@ -13,28 +13,30 @@ public class App
 {
     public static void main( String[] args )   {
         ManagePersonalImpl managePersonal = new ManagePersonalImpl();
+        ManagePersonalUI managePersonalUI = new ManagePersonalUI(managePersonal);
+        managePersonalUI.start();
 
-        SalesMan salesMan = new SalesMan("Dominik", "Ocsofszki", 90133);
-        final SocialAttribute socialAttribute = new SocialAttribute("Leadership Competence",4, 3, 20,"whats wrong?");
-        final SocialAttribute socialAttribute2 = new SocialAttribute("Openness to Employee",4, 3, 20,"whats wrong?");
-        final ArrayList<SocialAttribute> socialAttributes = new ArrayList();
-        socialAttributes.add(socialAttribute);
-        socialAttributes.add(socialAttribute2);
-        final SocialPerfomanceRecord socialPerfomanceRecord = new SocialPerfomanceRecord(socialAttributes, 2_000);
-        final Order order1 = new Order("HooverGo", "Telekom AG", "A", 700, 20, "YES!");
-        final Order order2 = new Order("HooverGo", "Mayer", "B", 500, 10, "YES");
-        ArrayList<Order> orders = new ArrayList();
-        orders.add(order1);
-        orders.add(order2);
-        final OrderEvaluation orderEvaluation = new OrderEvaluation(orders, 1_000);
-        final EvaluationRecord evaluationRecord = new EvaluationRecord(socialPerfomanceRecord, orderEvaluation, 1, 3_000, 2023);
-        managePersonal._dropAllSalesMan();
-
-        managePersonal.createSalesMan(salesMan);
-        System.out.println("-------");
-        managePersonal.readSalesMan(90133);
-        System.out.println("---perfRecord:----");
-        managePersonal.addPerformanceRecord(evaluationRecord, 90133);
+//        SalesMan salesMan = new SalesMan("Dominik", "Ocsofszki", 90133);
+//        final SocialAttribute socialAttribute = new SocialAttribute("Leadership Competence",4, 3, 20,"whats wrong?");
+//        final SocialAttribute socialAttribute2 = new SocialAttribute("Openness to Employee",4, 3, 20,"whats wrong?");
+//        final ArrayList<SocialAttribute> socialAttributes = new ArrayList();
+//        socialAttributes.add(socialAttribute);
+//        socialAttributes.add(socialAttribute2);
+//        final SocialPerfomanceRecord socialPerfomanceRecord = new SocialPerfomanceRecord(socialAttributes, 2_000);
+//        final Order order1 = new Order("HooverGo", "Telekom AG", "A", 700, 20, "YES!");
+//        final Order order2 = new Order("HooverGo", "Mayer", "B", 500, 10, "YES");
+//        ArrayList<Order> orders = new ArrayList();
+//        orders.add(order1);
+//        orders.add(order2);
+//        final OrderEvaluation orderEvaluation = new OrderEvaluation(orders, 1_000);
+//        final EvaluationRecord evaluationRecord = new EvaluationRecord(socialPerfomanceRecord, orderEvaluation, 1, 3_000, 2023);
+//        managePersonal._dropAllSalesMan();
+//
+//        managePersonal.createSalesMan(salesMan);
+//        System.out.println("-------");
+//        managePersonal.readSalesMan(90133);
+//        System.out.println("---perfRecord:----");
+//        managePersonal.addPerformanceRecord(evaluationRecord, 90133);
 
     }
 }
---
+++ java/app/src/main/java/task/ManagePersonalUI.java
@@ -0,0 +1,113 @@
+package task;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class ManagePersonalUI {
+    private final ManagePersonal managePersonal;
+
+    public ManagePersonalUI(ManagePersonal managePersonal) {
+        this.managePersonal = managePersonal;
+    }
+
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Waehlen Sie eine Aktion:");
+            System.out.println("1. SalesMan erstellen");
+            System.out.println("2. Leistungsbericht hinzufuegen");
+            System.out.println("3. SalesMan lesen");
+            System.out.println("4. SalesMan suchen");
+            System.out.println("5. Leistungsbericht lesen");
+            System.out.println("0. Beenden");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume the newline character
+
+            switch (choice) {
+                case 1:
+                    createSalesMan(scanner);
+                    break;
+                case 2:
+                    addPerformanceRecord(scanner);
+                    break;
+                case 3:
+                    readSalesMan(scanner);
+                    break;
+                case 4:
+                    querySalesMan(scanner);
+                    break;
+                case 5:
+                    readEvaluationRecords(scanner);
+                    break;
+                case 0:
+                    System.out.println("Anwendung wird beendet.");
+                    return;
+                default:
+                    System.out.println("Ungültige Eingabe. Bitte erneut eingeben.");
+            }
+        }
+    }
+
+    private void createSalesMan(Scanner scanner) {
+        System.out.println("Geben Sie den Vorname, Nachname und die ID des SalesMan ein (getrennt durch Leerzeichen):");
+        String userInput = scanner.nextLine();
+
+        String[] inputParts = userInput.split(" ");
+
+        if (inputParts.length != 3) {
+            System.out.println("Ungültige Eingabe. Bitte Vorname, Nachname und ID getrennt durch Leerzeichen eingeben.");
+            return;
+        }
+
+        String vorname = inputParts[0];
+        String nachname = inputParts[1];
+        int id;
+
+        try {
+            id = Integer.parseInt(inputParts[2]);
+        } catch (NumberFormatException e) {
+            System.out.println("Ungueltige ID. Bitte geben Sie eine ganze Zahl fuer die ID ein.");
+            return;
+        }
+
+        SalesMan salesMan = new SalesMan(vorname, nachname, id);
+        managePersonal.createSalesMan(salesMan);
+    }
+
+    private void addPerformanceRecord(Scanner scanner) {
+        System.out.println("This functionality is not implemented yet.\n");
+    }
+
+    private void readSalesMan(Scanner scanner) {
+        System.out.println("Geben Sie die SalesMan-ID ein:");
+        int sid = scanner.nextInt();
+        scanner.nextLine(); // Consume the newline character
+        SalesMan salesMan = managePersonal.readSalesMan(sid);
+        if (salesMan != null) {
+            System.out.println("SalesMan gefunden: " + salesMan.toString());
+        } else {
+            System.out.println("SalesMan nicht gefunden.");
+        }
+    }
+
+    private void querySalesMan(Scanner scanner) {
+        System.out.println("Geben Sie das Attribut und den Schlüsselwert für die Suche ein:");
+        String attribute = scanner.nextLine();
+        String key = scanner.nextLine();
+        List<SalesMan> salesMen = managePersonal.querySalesMan(attribute, key);
+        if (!salesMen.isEmpty()) {
+            System.out.println("Gefundene SalesMen:");
+            for (SalesMan salesMan : salesMen) {
+                System.out.println(salesMan.toString());
+            }
+        } else {
+            System.out.println("Keine SalesMen gefunden.");
+        }
+    }
+
+    private void readEvaluationRecords(Scanner scanner) {
+        System.out.println("This functionality is not implemented yet.\n");
+    }
+}
