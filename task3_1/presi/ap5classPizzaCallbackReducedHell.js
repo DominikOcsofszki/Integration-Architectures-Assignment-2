4522af12d98e0c12ee4a2fbbcaae33e43fba8545
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2023-11-21 13:33:01 +0100

    refactoring

--- task3_1/presi/ap5classPizzaCallbackReducedHell.js
+++ task3_1/presi/ap5classPizzaCallbackReducedHell.js
@@ -7,15 +7,28 @@ function callbackHandlerTimeout(callbackFunction,text, time) {
     setTimeout(() => callbackFunction(text), time);
 }
 
-callbackHandlerTimeout(callback,"1. item", 2000);
-callbackHandlerTimeout(callback,"2. item", 1000);
-callbackHandlerTimeout(callback,"3. item", 3000);
-callbackHandlerTimeout(callback,"4. item", 1000);
+// callbackHandlerTimeout(callback,"1. item", 2000);
+// callbackHandlerTimeout(callback,"2. item", 1000);
+// callbackHandlerTimeout(callback,"3. item", 3000);
+// callbackHandlerTimeout(callback,"4. item", 1000);
 // console.log("3. item");
 
-setTimeout(function () {
-    console.log("1. item");
-    setTimeout(function () {
-      callback("2. item");
-    }, 3000);
-  }, 3000);
+const hell = () => setTimeout(function () {
+    console.log("1. item");setTimeout(function () {
+        console.log("2. item");setTimeout(function () {
+            console.log("3. item");setTimeout(function () {
+                console.log("4. item");setTimeout(function () {
+                    console.log("5. item");setTimeout(function () {
+                        console.log("6. item");setTimeout(function () {
+                            console.log("7. item");setTimeout(function () {
+                                console.log("8. item");
+                            }, 3000);
+                        }, 1000);
+                    }, 500);
+                }, 500);
+            }, 500);
+        }, 500);
+    }, 500);
+}, 500);
+
+hell();
